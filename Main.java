fab297024ab8b023deb64cfe54877d1d50017382
Vraj Patel <69670615+Vraj5006@users.noreply.github.com>
2023-03-23 15:20:51 +0530

    updated main.java file from the github

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args){
+	    int a=0;
+	    int b=0;
+	System.out.println(a+" "+b);    
         System.out.println("Hello java");
 	System.out.println("Hello this is the change for firstr commit int the github");
 	System.out.println("Bye");
